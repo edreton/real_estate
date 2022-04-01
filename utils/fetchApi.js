d9b22f31b402f9ce92d9fb4ce9dbd09d07e22c85
edreton <edgar.retontali@gmail.com>
2022-03-31 18:11:57 -0600

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// headers: {
+//     'x-rapidapi-host': 'bayut.p.rapidapi.com',
+//     'x-rapidapi-key': '7a938757e1mshaa9cab91abeaf42p19ebdcjsn4f59b40b36cd'
+//   }
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+export const fetchApi = async(url) => {
+    const response = await axios.get(url, {
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': '7a938757e1mshaa9cab91abeaf42p19ebdcjsn4f59b40b36cd'
+        }
+    });
+    return response.data;
+}
