00b14ca17170d1b0ab9248923f5ce5f9f2de2fa9
Edgar Retontali <102101935+edreton@users.noreply.github.com>
2022-05-30 16:35:51 -0500

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -11,7 +11,7 @@ export const fetchApi = async(url) => {
     const response = await axios.get(url, {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '7a938757e1mshaa9cab91abeaf42p19ebdcjsn4f59b40b36cd'
+            'x-rapidapi-key': 'xxx'
         }
     });
     return response.data;
